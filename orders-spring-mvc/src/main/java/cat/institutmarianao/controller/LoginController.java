52dc2e100722991f425d44ddfef03ad8674f28f7
paumaso <162144281+paumaso@users.noreply.github.com>
2024-12-14 19:38:30 +0100

    14.12.24

--- orders-spring-mvc/src/main/java/cat/institutmarianao/controller/LoginController.java
+++ orders-spring-mvc/src/main/java/cat/institutmarianao/controller/LoginController.java
@@ -2,13 +2,19 @@ package cat.institutmarianao.controller;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 
 // TODO - Configure Spring element and add mappings
+@Controller
+@RequestMapping("/orders-spring-mvc")
 public class LoginController {
+	@GetMapping
 	public String check(HttpServletRequest request) throws ServletException, IOException {
 		if (request.isUserInRole("ROLE_ADMIN")) {
 			return "redirect:/admin/orders";
@@ -16,10 +22,12 @@ public class LoginController {
 		return "redirect:/users/orders";
 	}
 
+	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
 
+	@GetMapping("/loginfailed")
 	public String loginFailed(Model model) {
 		model.addAttribute("error", "true");
 		return "login";
